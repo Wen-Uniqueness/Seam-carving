97b044d60d0cdafbe4e525a444f3c4f67c6dc0ce
白日梦境 <145457822+Wen-Uniqueness@users.noreply.github.com>
2024-05-25 16:53:09 +0800

    整合

--- src/back_end/Stablize_SC.java
+++ src/back_end/Stablize_SC.java
@@ -3,38 +3,49 @@ package back_end;
 import edu.princeton.cs.algs4.Picture;
 
 public class Stablize_SC {
-    public static Picture shrinking(Picture picture, int new_width, int new_height){
+    public static Picture shrinking(Picture picture, int new_width, int new_height) {
         return (new SeamCarver(picture)).shrinking(new_width, new_height).picture();
     }
 
-    public static Picture expandingWidth(Picture picture, int new_width){
+    public static Picture expandingWidth(Picture picture, int new_width) {
         return new SeamCarver(picture).expandingWidth(new_width);
     }
 
-    public static Picture expandingHeight(Picture picture, int new_height){
+    public static Picture expandingHeight(Picture picture, int new_height) {
         return new SeamCarver(picture).expandingHeight(new_height);
     }
 
-    public static Picture shinking(Picture picture, int[][] protect, int new_width, int new_height){
+    public static Picture shrinking(Picture picture, int[][] protect, int new_width, int new_height) {
         return new SeamCarver(picture, protect).shrinking(new_width, new_height).picture();
     }
 
-    public static Picture expandingWidth(Picture picture, int[][] protect, int new_width){
+    public static Picture expandingWidth(Picture picture, int[][] protect, int new_width) {
         return new SeamCarver(picture, protect).expandingWidth(new_width);
     }
 
-    public static Picture expandingHeight(Picture picture, int[][] protect, int new_height){
+    public static Picture expandingHeight(Picture picture, int[][] protect, int new_height) {
         return new SeamCarver(picture, protect).expandingHeight(new_height);
     }
 
-    public static Picture bs(Picture picture, int new_width, int new_height){
-        if (new_height>picture.height()){
-            picture=expandingHeight(picture,new_height);
+    public static Picture bs(Picture picture, int[][] protect, int new_width, int new_height) {
+        if (new_height > picture.height()) {
+            picture = expandingHeight(picture, protect, new_height);
         }
-        if (new_width> picture.width()){
-            picture=expandingWidth(picture,new_width);
+        if (new_width > picture.width()) {
+            picture = expandingWidth(picture, protect, new_width);
         }
-        picture=shrinking(picture,new_width,new_height);
+        picture = shrinking(picture, protect, new_width, new_height);
+        return picture;
+    }
+
+    public static Picture bs(Picture picture, int new_width, int new_height) {
+        if (new_height > picture.height()) {
+            picture = expandingHeight(picture, new_height);
+        }
+        if (new_width > picture.width()) {
+            picture = expandingWidth(picture, new_width);
+        }
+        picture = shrinking(picture, new_width, new_height);
         return picture;
     }
 }
